536a3faac83f5df8e9ed8a45fdb55ae13e863600
Dola Ram <42748067+dolaram@users.noreply.github.com>
2018-09-07 09:37:49 +0000

    Delete randInitializeWeights.py

--- randInitializeWeights.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def randInitializeWeights(L_in, L_out):
-    W = np.zeros((L_out, 1 + L_in))
-    epsilon_init = 0.12
-    W = np.random.rand(L_out, 1 + L_in) * 2 * epsilon_init- epsilon_init
-    return W
